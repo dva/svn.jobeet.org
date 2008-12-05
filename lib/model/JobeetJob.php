d9ec9224247ad2504984abfbe3b2e534e4c4f9ce
Stefan.Koopmanschap <Stefan.Koopmanschap@feb49302-487e-464a-b2d3-c0355f879f62>
2008-12-05 13:43:12 +0000

    day 5 changes

--- lib/model/JobeetJob.php
+++ lib/model/JobeetJob.php
@@ -2,6 +2,21 @@
 
 class JobeetJob extends BaseJobeetJob
 {
+  public function getCompanySlug()
+  {
+    return Jobeet::slugify($this->getCompany());
+  }
+
+  public function getPositionSlug()
+  {
+    return Jobeet::slugify($this->getPosition());
+  }
+
+  public function getLocationSlug()
+  {
+    return Jobeet::slugify($this->getLocation());
+  }
+  
   public function __toString()
   {
     return sprintf('%s at %s (%s)', $this->getPosition(), $this->getCompany(), $this->getLocation());
