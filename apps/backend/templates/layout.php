f4bd5b63c3db76e5a839736de33bd02d1a78a8f2
Stefan.Koopmanschap <Stefan.Koopmanschap@feb49302-487e-464a-b2d3-c0355f879f62>
2008-12-11 19:49:11 +0000

    day 13 changes

--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -17,14 +17,16 @@
           </a>
         </h1>
       </div>
- 
+      <?php if ($sf_user->isAuthenticated()) : ?>
       <div id="menu">
         <ul>
           <li><?php echo link_to('Jobs', '@jobeet_job') ?></li>
           <li><?php echo link_to('Categories', '@jobeet_category') ?></li>
+          <li><?php echo link_to('Users', '@sf_guard_user') ?></li>
+          <li><?php echo link_to('Logout', '@sf_guard_signout') ?></li>
         </ul>
       </div>
- 
+      <?php endif; ?>
       <div id="content">
         <?php echo $sf_content ?>
       </div>
